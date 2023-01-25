23fdc1a6b4ed9d12a587be573eb7f206a50cd3ef
kush.pachghare <kush.pachghare@intelliquity.in>
2023-01-25 16:51:35 +0530

    First Commit

---
+++ ConsoleApp_Client_WorkingWithServer/ConsoleApp_Client_WorkingWithServer/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcService1_SimpleTalkGrpcService;
+
+Console.WriteLine("Hello, World!");
+using var channel = GrpcChannel.ForAddress("https://localhost:7176");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ SimpleTalkGrpcWebClient/SimpleTalkGrpcWebClient/Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using Grpc.Net.Client;
+using ProductsService;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SimpleTalkGrpcWebClient.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public ProductsController() 
+        {
+            channel = GrpcChannel.ForAddress("https://localhost:7176");
+        }
+
+        [HttpGet]
+        public List<Product> GetAll()
+        {
+            var client = new ProductService.ProductServiceClient(channel);
+            return client.GetAll(new Empty()).Products.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public IActionResult GetById(int id)
+        {
+            var client = new ProductService.ProductServiceClient(channel);
+            var product = client.Get(new ProductId { Id = id });
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(product);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            var client = new ProductService.ProductServiceClient(channel);
+            var createdProduct = client.Insert(product);
+
+            return CreatedAtRoute("GetProduct", new { id = createdProduct.ProductId }, createdProduct);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Product product)
+        {
+            var client = new ProductService.ProductServiceClient(channel);
+            var udpatedProduct = client.Update(product);
+            if (udpatedProduct == null)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var client = new ProductService.ProductServiceClient(channel);
+            client.Delete(new ProductId { Id = id });
+            return new ObjectResult(id);
+        }
+    }
+}
